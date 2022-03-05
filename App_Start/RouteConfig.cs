665669dac3d009bd66c2e29b9c3b7ef98e737d2b
magatzemgaillard <magatzemgaillard@outlook.es>
2022-03-05 11:37:06 +0100

    Rectifica "RouteConfig"

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,12 +16,11 @@ namespace CasaGaillard
             //Enabling attribute routing 
             //routes.MapMvcAttributeRoutes();
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new {controller = "Pagina", action = "Index", id = UrlParameter.Optional },
-                namespaces: new [] {"CasaGaillard.controllers"}
-            );
+            //routes.MapRoute(
+                //name: "Default",
+                //url: "{controller}/{action}/{id}",
+                //defaults: new {action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
